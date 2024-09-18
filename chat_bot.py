be0b890a46ee54b34551d931cc05e4a6b454f086
Pranjal Gupta <156123068+pranjal9084@users.noreply.github.com>
2024-09-18 21:04:04 +0530

    Update chat_bot.py
    changed the model from gemma-7b-it to llama-3.1-70b-versatile

--- chat_bot.py
+++ chat_bot.py
@@ -138,7 +138,7 @@ if user_prompt:
 
     # Get response from the LLM
     response = client.chat.completions.create(
-        model="gemma-7b-it",
+        model="llama-3.1-70b-versatile",
         messages=messages
     )
 
